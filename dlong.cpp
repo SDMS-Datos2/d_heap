2d94756a0be3dbe5d7a4ffa322322a6f893189a1
nitsuj07 <justinjosue01@gmail.com>
2015-10-02 22:15:04 -0600

    Create dlong.cpp

---
+++ dlong.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   dlong.cpp
+ * Author: justin
+ *
+ */
+
+#include "dlong.h"
+#include <dheap.h>
+
+/**
+ * 
+ * @param pPunterodHeap
+ */
+
+dLong::dLong(void* pPunterodHeap)
+{
+    punteroHeap = pPunterodHeap;
+}
+
+/**
+ * @brief dLong::operator = asignacion
+ * Guarda el dato remotamente y lo borra localmente
+ * @param pLong un dato tipo long.
+ * @return un puntero tipo void.
+ */
+void* dLong::operator =(const long pLong){
+    long _dato = pLong;
+    d_pointer_size_type _puntero;
+    _puntero= ((dHeap*)punteroHeap)->dMalloc(_dato, "String");
+    ((dHeap*)punteroHeap)->dnew(_puntero,_dato);
+}
